5c4add4a83ce88fa5f44bd3b3d65d31f0156667c
PALLAVI-96 <pallavisuresh96@gmail.com>
2025-05-09 15:51:11 +0530

    Simple node app

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const axios=require("axios")
-console.log("Hello, welcome to the world")
-console.log("Hello, welcome to the world")
+// app.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Root route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Root route
+app.get('/pallavi', (req, res) => {
+    res.send('Hello, Pallavi !');
+  });
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
